f4fe3217f60811d9e4adce555df2ee2a511a9851
Tu <tuphan694@gmail.com>
2023-06-03 09:41:32 +0700

    build UI header #1

--- src/components/Layout/components/Header/index.jsx
+++ src/components/Layout/components/Header/index.jsx
@@ -1,5 +1,8 @@
 import styles from './Header.module.scss'
 import classNames from 'classnames/bind';
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles)
 
@@ -7,7 +10,22 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt='logo'/>
+                </div> 
+                <form className={cx('search')}>
+                    <input type='text' name='search' placeholder='Search accounts or videos' spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark}/>
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </form>
+                <div className={cx('actions')}>
+                    
+                </div>
             </div>
         </header>
     );
